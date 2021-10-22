65289b55f7687a561dcf8aae5331d6f94ba8089e
Blake Estes <blake.estes@protonmail.com>
2021-10-22 11:57:09 -0700

    init -- CRA/typescript/prettier/tests

---
+++ src/App.test.tsx
@@ -0,0 +1,25 @@
+import { shallow } from "enzyme";
+import { App } from "./App";
+import { render } from "@testing-library/react";
+
+describe("App", () => {
+  describe("using enzyme", () => {
+    it("returns the text", () => {
+      const component = shallow(<App />);
+
+      expect(component.find('[data-testid="greetings-container"]').text()).toBe(
+        "Greetings! Welcome to the XQ take-home assessment"
+      );
+    });
+  });
+
+  describe("using testing library", () => {
+    it("returns the text", () => {
+      const component = render(<App />);
+
+      expect(component.getByTestId("greetings-container").innerHTML).toBe(
+        "Greetings! Welcome to the XQ take-home assessment"
+      );
+    });
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function App(): JSX.Element {
+  return (
+    <div data-testid="greetings-container">
+      Greetings! Welcome to the XQ take-home assessment
+    </div>
+  );
+}

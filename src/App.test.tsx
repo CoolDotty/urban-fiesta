299e71af5e057e7490b2ff2d2d8c687084b6da2d
Karl Phillips <k@rlphilli.ps>
2023-04-02 06:37:25 +0000

    add simple App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,6 +10,19 @@ describe("App", () => {
       expect(component.find('[data-testid="greetings-container"]').text()).toBe(
         "Greetings! Welcome to the XQ take-home assessment"
       );
+      
+      // Can see login box when loaded in
+      expect(component.find('fieldset > legend').text()).toBe(
+        "Login"
+      );
+
+      // Cannot see decrypt or encrypt boxes (because not logged in)
+      expect(component.find('fieldset > legend').text()).not.toBe(
+        "Encrypt"
+      );
+      expect(component.find('fieldset > legend').text()).not.toBe(
+        "Decrypt"
+      );
     });
   });
 
